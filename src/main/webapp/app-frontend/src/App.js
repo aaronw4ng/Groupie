9c076e19a30b3b028386166d0036490fd3782e75
Aaron Wang <wangaaro@usc.edu>
2021-10-04 13:04:53 -0700

    Initial commit

---
+++ src/main/webapp/app-frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import AccountCreation from './AccountCreation';
+import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    console.log("Host URL"+process.env.PUBLIC_URL);
+    return (
+
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Scheduling App</h1>
+        </header>
+        <AccountCreation></AccountCreation>
+          {/* <Switch>
+                <Route exact path= "/" render={() => (
+                  <Redirect to="/customerlist"/>
+                )}/>
+                 <Route exact path='/customerlist' component={Customers} />
+          </Switch> */}
+
+      </div>
+    </Router>
+    );
+  }
+}
+
+export default App;
