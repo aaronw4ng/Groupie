845b50ab65e24e0b08ad20b0b7f306197d698de4
Jiating Lu <jiatinglu99@gmail.com>
2021-10-04 14:54:34 -0700

    added basic function shells and test functions for database connection

--- src/main/java/csci310/Database.java
+++ src/main/java/csci310/Database.java
@@ -6,21 +6,39 @@ public class Database {
 	
 	public Database(){
 		try{
-			connection = DriverManager.getConnection("jdbc:mariadb://maria_db:3306/when2meet", "root", "csci310project");
+			connection = DriverManager.getConnection("jdbc:mariadb://maria_db:3306/project27", "root", "csci310project");
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+
+	// check if a particular table exists in the database
+	public Boolean checkTableExists(String tableName) {
+		return false;
+	}
+
+	// initialize all required tables in the database
+	public Boolean createRequiredTables(){
+		return false;
+	}
+
+	// drop all tables
+	public Boolean dropAllTables(){
+		return false;
+	}
 	
+	// add user and hashed password to the table
 	public Boolean register(String _us, String _pd) {
 		return false;
 	}
 	
+	// check if hashed password matches with stored hashed password in the DB
 	public Boolean login(String _us, String _pd) {
 		return false;
 	}
 	
+	// remove the according user from table
 	public Boolean deactivate(String _us, String _pd) {
 		return false;
 	}
--- src/test/java/csci310/DatabaseTest.java
+++ src/test/java/csci310/DatabaseTest.java
@@ -6,20 +6,49 @@ import org.junit.Test;
 
 public class DatabaseTest {
 	@Test
-	public void testDatabase() {
+	public void testCheckTableExists(){
 		Database testDB = new Database();
+
+		assertTrue(testDB.checkTableExists("users") == false);
+		testDB.createRequiredTables();
+		assertTrue(testDB.checkTableExists("users"))
+
+		testDB.dropAllTables();
+	}
+
+	@Test
+	public void testCreateRequiredTables(){
+		Database testDB = new Database();
+
+		assertTrue(testDB.checkTableExists("users") == false);
+		testDB.createRequiredTables();
+		// TODO: check if all required tables exist
+		assertTrue(testDB.checkTableExists("users"))
+
+		testDB.dropAllTables();
 	}
 
 	@Test
 	public void testRegister() {
 		Database testDB = new Database();
+
 		assertTrue(testDB.register("dummy_user", "password"));
+		assertTrue(testDB.testLogin("dummy_user", "password"));
+		// TODO: test more invalid passwords
+
+		testDB.dropAllTables();
 	}
 
 	@Test
 	public void testLogin() {
 		Database testDB = new Database();
+
+		assertTrue(testDB.register("dummy_user", "password"));
 		assertTrue(testDB.login("dummy_user", "password"));
+		assertTrue(testDB.login("dummy_user", "notpassword") == false);
+		assertTrue(testDB.login("notdummy_user", "password") == false);
+
+		testDB.dropAllTables();
 	}
 	
 	@Test
