78cbe083e57746826a034d8873b2591b14ac04ca
Karen Ly <karenly@usc.edu>
2021-10-14 15:31:47 -0700

    Remove sample source code: GreetingServlet, Hello class and jsp, HelloTest.java, and hello.feature

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -19,26 +19,6 @@ public class StepDefinitions {
 
 	private final WebDriver driver = new ChromeDriver();
 
-	@Given("I am on the index page")
-	public void i_am_on_the_index_page() {
-		driver.get(ROOT_URL);
-	}
-
-	@When("I click the link {string}")
-	public void i_click_the_link(String linkText) {
-		driver.findElement(By.linkText(linkText)).click();
-	}
-
-	@Then("I should see header {string}")
-	public void i_should_see_header(String header) {
-		assertTrue(driver.findElement(By.cssSelector("h2")).getText().contains(header));
-	}
-	
-	@Then("I should see text {string}")
-	public void i_should_see_text(String text) {
-		assertTrue(driver.getPageSource().contains(text));
-	}
-
 	@After()
 	public void after() {
 		driver.quit();
