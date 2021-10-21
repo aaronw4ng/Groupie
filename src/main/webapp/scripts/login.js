a277aa283c32f46f1461e6746224b7a0ff7900bf
Aaron Wang <wangaaro@usc.edu>
2021-10-20 17:30:43 -0700

    Rectified register and login scripts

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -3,7 +3,7 @@ function handleLoginClick(event) {
     const usernameInput = document.querySelector("#input-username").value
     const passwordInput = document.querySelector("#input-password").value
 
-    console.log(usefrnameInput + " " + passwordInput)
+    console.log(usernameInput + " " + passwordInput)
     
     console.log("Logging in")
 
