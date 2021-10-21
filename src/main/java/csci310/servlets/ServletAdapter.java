504ded85b69687492a6bda0e7016ce1fd759a9f9
Jiating Lu <jiatinglu99@gmail.com>
2021-10-20 23:10:40 -0700

    implemented servletAdapter basic functions

--- src/main/java/csci310/servlets/ServletAdapter.java
+++ src/main/java/csci310/servlets/ServletAdapter.java
@@ -7,9 +7,26 @@ import javax.servlet.http.*;
 public class ServletAdapter extends HttpServlet{
     public Database database = null;
     // initialize the database connection to be shared in application
+    // @Override
     public void init() throws ServletException{
+        database = (Database)getServletConfig().getServletContext().getAttribute("database");
+        if (database == null){
+            try{
+                // create a new shared instance of database
+                database = new Database("test.db");
+                // set it on the application level
+                getServletConfig().getServletContext().setAttribute("database", database);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
     // close the application-wise database connection and set it to null
     public void close() throws Exception{
+        // remove from application context
+        getServletConfig().getServletContext().removeAttribute("database");
+        database.close();
+        database = null;
     }
 }
--- src/test/java/csci310/servlets/ServletAdapterTest.java
+++ src/test/java/csci310/servlets/ServletAdapterTest.java
@@ -8,10 +8,11 @@ public class ServletAdapterTest {
     @Test
     public void testInit() throws Exception{
         ServletAdapter testAdapter = new ServletAdapter();
+        testAdapter.init();
         assertNotNull(testAdapter.database);
         testAdapter.close();
     }
-    
+
     @Test
     public void testClose() throws Exception{
         ServletAdapter testAdapter = new ServletAdapter();
