3e77c0ce5f390ef3cdb8e67139515c81ed25992b
Jiating Lu <jiatinglu99@gmail.com>
2021-10-27 02:03:00 -0700

    implemented adapter class

--- src/main/java/csci310/servlets/ServletAdapter.java
+++ src/main/java/csci310/servlets/ServletAdapter.java
@@ -2,23 +2,28 @@ package csci310.servlets;
 
 import csci310.Database;
 import javax.servlet.ServletException;
+import javax.servlet.ServletConfig;
 import javax.servlet.http.*;
 
 public class ServletAdapter extends HttpServlet{
     public Database database = null;
+    public static String db_name = "project27.db";
+    public static String db_config = "db_config.ini";
+    public static String db_pass = "ThisProjectIsSoMuchFun";
     // initialize the database connection to be shared in application
-    // @Override
-    public void init() throws ServletException{
+    @Override
+    public void init(ServletConfig config) throws ServletException{
+        super.init(config);
         database = (Database)getServletConfig().getServletContext().getAttribute("database");
         if (database == null){
             try{
                 // create a new shared instance of database
-                database = new Database("test.db");
+                database = new Database(db_name, db_config, db_pass);
                 // set it on the application level
                 getServletConfig().getServletContext().setAttribute("database", database);
             }
             catch (Exception e){
-                e.printStackTrace();
+                System.out.println("failed to establish database connection");
             }
         }
     }
--- src/test/java/csci310/servlets/ServletAdapterTest.java
+++ src/test/java/csci310/servlets/ServletAdapterTest.java
@@ -3,19 +3,51 @@ package csci310.servlets;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class ServletAdapterTest {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletConfig;
+import org.junit.Before;
+import org.mockito.Mockito;
 
+public class ServletAdapterTest {
+    private ServletConfig config;
+    private ServletContext context;
+
+    @Before
+    public void setUp() throws Exception {
+        ServletAdapter.db_name = "test.db";
+        config = Mockito.mock(ServletConfig.class);
+        context = Mockito.mock(ServletContext.class);
+        Mockito.doReturn(context).when(config).getServletContext();
+    }
+    
     @Test
     public void testInit() throws Exception{
         ServletAdapter testAdapter = new ServletAdapter();
-        testAdapter.init();
+        testAdapter.init(config);
         assertNotNull(testAdapter.database);
-        testAdapter.close();
+        // mock shared database connection returned by context
+        Mockito.doReturn(testAdapter.database).when(context).getAttribute("database");
+        ServletAdapter testAdapter2 = new ServletAdapter();
+        testAdapter2.init(config);
+        assertNotNull(testAdapter2.database);
+    }
+
+    @Test
+    public void testInitWrongPass() throws Exception{
+        // test wrong db_pass for coverage
+        String temp = ServletAdapter.db_pass;
+        ServletAdapter.db_pass = "wrongPassword";
+        ServletAdapter testAdapter = new ServletAdapter();
+        testAdapter.init(config);
+        assertNull(testAdapter.database);
+        // revert back to original password
+        ServletAdapter.db_pass = temp;
     }
 
     @Test
     public void testClose() throws Exception{
         ServletAdapter testAdapter = new ServletAdapter();
+        testAdapter.init(config);
         testAdapter.close();
         assertNull(testAdapter.database);
     }
