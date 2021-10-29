f4cb5442c1f3953c2de13092302e736441bc7f22
Karen Ly <karenly@usc.edu>
2021-10-28 20:13:56 -0700

    Add shell and test for searchEvents function

---
+++ src/main/java/csci310/Ticketmaster.java
@@ -0,0 +1,14 @@
+package csci310;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+import com.google.gson.*;
+
+public class Ticketmaster {
+    // search up event through keyword
+    String searchEvents(String keyword) throws IOException {
+       return "All results related to " + keyword;
+    }
+}
---
+++ src/test/java/csci310/TicketmasterTest.java
@@ -0,0 +1,22 @@
+package csci310;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TicketmasterTest {
+    Ticketmaster ticketmaster;
+
+    @Before
+    public void setUp() throws Exception {
+        ticketmaster = new Ticketmaster();
+    }
+
+    @Test
+    public void testSearchEvents() throws Exception {
+        String result = ticketmaster.searchEvents("BTS");
+        assertEquals("All results related to BTS", result);
+        System.out.println(result);
+    }
+}
