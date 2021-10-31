cc0e0d8f46e5f7a92b5c99312cc2fb0e101530eb
Jiating Lu <jiatinglu99@gmail.com>
2021-10-31 16:01:36 -0700

    fully implemented AppServletContextListener and its tests

--- src/test/java/csci310/AppServletContextListenerTest.java
+++ src/test/java/csci310/AppServletContextListenerTest.java
@@ -1,14 +1,10 @@
 package csci310;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
-
-import org.junit.Before;
 import org.mockito.Mockito;
 
 public class AppServletContextListenerTest {
@@ -19,8 +15,29 @@ public class AppServletContextListenerTest {
         AppServletContextListener listener = new AppServletContextListener();
         ServletContextEvent event = new ServletContextEvent(context);
         listener.contextInitialized(event);
+        assertTrue(listener.database != null);
+        // mock shared database connection returned by context
+        Mockito.doReturn(listener.database).when(context).getAttribute("database");
         assertTrue(context.getAttribute("database") != null);
         listener.contextDestroyed(event);
+    }
+    
+    @Test
+    public void testContextInitializedWrongPass(){
+        // test wrong password for the database
+        String temp = AppServletContextListener.db_pass;
+        AppServletContextListener.db_pass = "wrongPassword";
+        ServletContext context = Mockito.mock(ServletContext.class);
+        AppServletContextListener listener = new AppServletContextListener();
+        ServletContextEvent event = new ServletContextEvent(context);
+        listener.contextInitialized(event);
+        assertTrue(listener.database == null);
+        // mock shared database connection returned by context
+        Mockito.doReturn(listener.database).when(context).getAttribute("database");
+        assertTrue(context.getAttribute("database") == null);
+        listener.contextDestroyed(event);
+        // reset password
+        AppServletContextListener.db_pass = temp;
     }
 
     @Test
@@ -30,6 +47,9 @@ public class AppServletContextListenerTest {
         ServletContextEvent event = new ServletContextEvent(context);
         listener.contextInitialized(event);
         listener.contextDestroyed(event);
+        assertTrue(listener.database == null);
+        // mock shared database connection returned by context
+        Mockito.doReturn(listener.database).when(context).getAttribute("database");
         assertTrue(context.getAttribute("database") == null);
     }
 }
