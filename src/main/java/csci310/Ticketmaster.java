52bbe5ee0db3b3e11375f70677f0d7774e96c4b0
Karen Ly <karenly@usc.edu>
2021-10-31 17:06:13 -0700

    Change searchEvents to have almost everything in a try, catch block in case that we get null exception when trying to parse some of the data or connection exception (e.g. can't reach api because no internet)

--- src/main/java/csci310/Ticketmaster.java
+++ src/main/java/csci310/Ticketmaster.java
@@ -28,54 +28,54 @@ public class Ticketmaster {
         }
         // add api key to query at the very end after all parameters
         host = host + "&apikey=" + api_key;
-        URL url = new URL(host);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.connect();
-
-        Scanner scanner = new Scanner(url.openStream());
-
-        //Write all the JSON data into a string using a scanner
-        while (scanner.hasNext()) {
-            result += scanner.nextLine();
-        }
-        //Close the scanner
-        scanner.close();
-
-
-        // turn into json object in order to extract embedded items
-        JsonObject jobj;
-        String events;
         try {
+            URL url = new URL(host);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            Scanner scanner = new Scanner(url.openStream());
+
+            //Write all the JSON data into a string using a scanner
+            while (scanner.hasNext()) {
+                result += scanner.nextLine();
+            }
+            //Close the scanner
+            scanner.close();
+
+
+            // turn into json object in order to extract embedded items
+            JsonObject jobj;
+            String events;
             jobj = new Gson().fromJson(result, JsonObject.class);
             events = jobj.get("_embedded").toString();
+
+            // turn into json object again in order to array of events
+            JsonObject eventsObj = new Gson().fromJson(events, JsonObject.class);
+            JsonArray eventsArray = eventsObj.getAsJsonArray("events");
+
+            ArrayList<Event> refinedListOfEvents = new ArrayList<>();
+
+            // iterate through each event and get the name and url
+            for (int i = 0; i < eventsArray.size(); i++) {
+                // should check for null before parsing any data
+                String event = eventsArray.get(i).toString();
+                // turn each individual event into json object
+                JsonObject eventDetails = new Gson().fromJson(event, JsonObject.class);
+                // create a new event w/ name
+                Event newEvent = new Event(eventDetails.get("name").toString());
+                // get url of event
+                newEvent.setURLEvent(eventDetails.get("url").toString());
+                refinedListOfEvents.add(newEvent);
+            }
+            // convert the refined results of list into events back into json format
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String json = gson.toJson(refinedListOfEvents);
+            return json;
         }
-        // Results were empty aka no events found
+        // Results were empty aka no events found or something went wrong when trying to connect
         catch (Exception e) {
             throw new Exception("No results found!");
         }
-
-        // turn into json object again in order to array of events
-        JsonObject eventsObj = new Gson().fromJson(events, JsonObject.class);
-        JsonArray eventsArray = eventsObj.getAsJsonArray("events");
-
-        ArrayList<Event> refinedListOfEvents = new ArrayList<>();
-
-        // iterate through each event and get the name and url
-        for (int i = 0; i < eventsArray.size(); i++) {
-            // should check for null before parsing any data
-            String event = eventsArray.get(i).toString();
-            // turn each individual event into json object
-            JsonObject eventDetails = new Gson().fromJson(event, JsonObject.class);
-            // create a new event w/ name
-            Event newEvent = new Event(eventDetails.get("name").toString());
-            // get url of event
-            newEvent.setURLEvent(eventDetails.get("url").toString());
-            refinedListOfEvents.add(newEvent);
-        }
-        // convert the refined results of list into events back into json format
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String json = gson.toJson(refinedListOfEvents);
-        return json;
     }
 }
