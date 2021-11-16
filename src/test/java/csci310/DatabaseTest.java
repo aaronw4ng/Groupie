9b7901d8a151757a2578039aa328acc22d1b5914
Karen Ly <karenly@usc.edu>
2021-11-16 01:16:29 -0800

    Add additional test for remove invitee for sent proposal database function to get full coverage

--- src/test/java/csci310/DatabaseTest.java
+++ src/test/java/csci310/DatabaseTest.java
@@ -651,4 +651,43 @@ public class DatabaseTest {
 		testDB.dropAllTables();
 		testDB.close();
 	}
+
+	@Test
+	public void testRemoveInviteeFromSentProposalUserDNE() throws Exception {
+		Database testDB = new Database("test.db");
+		testDB.dropAllTables();
+		testDB.createRequiredTables();
+		// Create a proposal first
+		// add user to database first
+		testDB.register("Test User", "Test Password"); // userId = 1
+		String title = "Remove Invitee from Sent Proposal";
+		String descript = "This is a test description for removing an invitee after sending proposal test!";
+		List<String> invitees = new ArrayList<>();
+		invitees.add("Invitee 1");
+		invitees.add("Invitee 2");
+		// add invitees as users
+		testDB.register("Invitee 1", "PS1"); // userId = 2
+		testDB.register("Invitee 2", "PS2"); // userId = 3
+		List<Venue> venues1 = new ArrayList<>();
+		venues1.add(new Venue("birthdayVenue", "VenueAddress", "VenueCity", "VenueState", "VenueCountry"));
+		List<Venue> venues2 = new ArrayList<>();
+		venues2.add(new Venue("BTSConcertVenue", "VenueAddress", "VenueCity", "VenueState", "VenueCountry"));
+		List<Event> events = new ArrayList<>();
+		events.add(new Event("Birthday", "TestURL", "TestStartDate", venues1));
+		events.add(new Event("BTS Concert", "TestURL", "TestStartDate", venues2));
+		int newProposalId = testDB.savesDraftProposal("Test User", title, descript, invitees, events, true, -1);
+		assertEquals(1, newProposalId);
+
+		// Send the proposal
+		Boolean sentStatus = testDB.sendProposal(newProposalId);
+		assertEquals(true, sentStatus);
+
+		// Try to remove the nonexisting user from the sent proposal
+		assertEquals(3, testDB.queryUserID("Invitee 2"));
+		Boolean status = testDB.removeInviteeFromSentProposal(1, 4);
+		assertEquals(false, status);
+
+		testDB.dropAllTables();
+		testDB.close();
+	}
 }
