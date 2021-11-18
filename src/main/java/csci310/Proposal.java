f8910b282d12f8b8ee40d5ca72b63bb62a5e048b
Jiating Lu <jiatinglu99@gmail.com>
2021-11-17 22:52:31 -0800

    removing functionalities from Proposal class

--- src/main/java/csci310/Proposal.java
+++ src/main/java/csci310/Proposal.java
@@ -8,57 +8,27 @@ public class Proposal {
     public String title;
     public String description;
     public Boolean isDraft;
-    public Map<String, Event> events;
-    public Map<String, User> invitedUsers;
+    public List<Event> events;
+    public List<User> invitedUsers;
    // private Map<User, Response> mapOfResponses;
     public Event bestEvent;
-    public Map<String, User> accepted;
-    public Map<String, User> declined;
+    public List<User> accepted;
+    public List<User> declined;
 
     public Proposal(User user) {
         this.user = user;
-        events = new TreeMap<>();
-        invitedUsers = new TreeMap<>();
+        events = new ArrayList<>();
+        invitedUsers = new ArrayList<>();
         //mapOfResponses = new TreeMap<>();
-        accepted = new TreeMap<>();
-        declined = new TreeMap<>();
+        accepted = new ArrayList<>();
+        declined = new ArrayList<>();
     }
 
     public Proposal() {
-        events = new TreeMap<>();
-        invitedUsers = new TreeMap<>();
+        events = new ArrayList<>();
+        invitedUsers = new ArrayList<>();
         //mapOfResponses = new TreeMap<>();
-        accepted = new TreeMap<>();
-        declined = new TreeMap<>();
-    }
-
-    public User getOwner() {
-        return this.user;
-    }
-
-    public Map<String, Event> getEvents() {
-        return this.events;
-    }
-
-    public Map<String, User> getAllInvitedUsers() {
-        return this.invitedUsers;
-    }
-
-    public void addEvent(Event event) {
-        events.put(event.getEventName(), event);
-    }
-
-    public void removeEvent(Event event) {
-        // find event to be removed
-        events.remove(event.getEventName());
-    }
-
-    public void inviteUser(User user) {
-        invitedUsers.put(user.getUsername(), user);
-    }
-
-    public void removeUser(User user) {
-        // find user to be removed
-        invitedUsers.remove(user.getUsername());
+        accepted = new ArrayList<>();
+        declined = new ArrayList<>();
     }
 }
