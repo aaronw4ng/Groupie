4a1a07fa2e219966b028c910703c019c9a7e836b
Aaron Wang <wangaaro@usc.edu>
2021-11-18 11:38:00 -0800

    Added missing semicolon

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -81,7 +81,7 @@ public class StepDefinitions {
 
 	@When("user inputs random username in username") 
 	public void user_inputs_random_username_in_username() {
-		String username = RandomStringUtils.random(USERNAME_LENGTH, USERNAME_CHARACTERS)
+		String username = RandomStringUtils.random(USERNAME_LENGTH, USERNAME_CHARACTERS);
 		driver.findElement(By.id("input-username")).sendKeys(username);
 	}
 
