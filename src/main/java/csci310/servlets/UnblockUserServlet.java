e5dc37a5d9ae6f27ceaff0e8409f201e32d7d37f
Jiating Lu <jiatinglu99@gmail.com>
2021-11-23 20:56:33 -0800

    add skeleton code for all user-privacy related servlets

---
+++ src/main/java/csci310/servlets/UnblockUserServlet.java
@@ -0,0 +1,20 @@
+package csci310.servlets;
+
+import csci310.Database;
+import csci310.UserAvailability;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class UnblockUserServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    }
+}
---
+++ src/test/java/csci310/servlets/BlockUserServletTest.java
@@ -0,0 +1,74 @@
+package csci310.servlets;
+
+import csci310.AppServletContextListener;
+import csci310.Database;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class BlockUserServletTest {
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+
+    // for mocking server initialization
+    private static ServletContext context;
+    private static AppServletContextListener listener;
+    private static ServletContextEvent event;
+    private static ServletConfig config;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        // database initialization
+        AppServletContextListener.db_name = "test.db";
+        context = Mockito.mock(ServletContext.class);
+        listener = new AppServletContextListener();
+        event = new ServletContextEvent(context);
+        config = Mockito.mock(ServletConfig.class);
+        listener.contextInitialized(event);
+        Mockito.doReturn(listener.database).when(context).getAttribute("database");
+        Mockito.doReturn(context).when(config).getServletContext();
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        request = Mockito.mock(HttpServletRequest.class);
+        response = Mockito.mock(HttpServletResponse.class);
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+        listener.contextDestroyed(event);
+    }
+
+    @Test
+    public void testDoPost() throws Exception {
+        // clear the database
+        Database testDB = listener.database;
+        testDB.dropAllTables();
+        testDB.createRequiredTables();
+        
+        // register users
+        testDB.register("user1", "ps1");
+        testDB.register("user2", "ps2");
+        testDB.register("user3", "ps3");
+
+        fail();
+    }
+}
