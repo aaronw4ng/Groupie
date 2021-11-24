2b0afce404d378011a651d67b25fec42cd7258bd
Karen Ly <karenly@usc.edu>
2021-11-24 13:43:29 -0800

    Add additional tests for remove event from sent proposal servlet

--- src/test/java/csci310/servlets/RemoveEventFromSentProposalServletTest.java
+++ src/test/java/csci310/servlets/RemoveEventFromSentProposalServletTest.java
@@ -13,9 +13,11 @@ import org.mockito.Mockito;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
@@ -103,5 +105,73 @@ public class RemoveEventFromSentProposalServletTest {
         removeEventFromSentProposalServlet.doPost(request, response);
         String result = sw.getBuffer().toString();
         assertEquals("true", result);
+    }
+
+    @Test
+    public void testDoPostEventDoesNotExist() throws Exception {
+        // clear the database
+        Database testDB = listener.database;
+        testDB.dropAllTables();
+        testDB.createRequiredTables();
+
+        // register the user and invitees
+        testDB.register("TestUser", "TestPassword");
+        testDB.register("Invitee1", "ps1");
+        testDB.register("Invitee2", "ps2");
+
+        testDB.register("TestUser", "TestPassword");
+        testDB.register("Invitee1", "ps1");
+        testDB.register("Invitee2", "ps2");
+
+        // Adding the proposal draft
+        String title = "My Old Test Proposal for Remove Event from Sent Proposal Servlet";
+        String descript = "This is a test description for remove event from sent proposal servlet!";
+        List<String> invitees = new ArrayList<>();
+        invitees.add("Invitee1"); // user Id is 2
+        invitees.add("Invitee2"); // user Id is 3
+        List<Event> events = new ArrayList<>();
+        List<Venue> venues = new ArrayList<>();
+        venues.add(new Venue("VenueName1", "VenueAddress1", "VenueCity1", "VenueState1", "VenueCountry1"));
+        venues.add(new Venue("VenueName2", "VenueAddress2", "VenueCity2", "VenueState2", "VenueCountry2"));
+        events.add(new Event("TestEvent1", "TestURL1", "TestStartDate1", venues)); // event Id 1
+        events.add(new Event("TestEvent2", "TestURL2", "TestStartDate2", venues)); // event Id 2
+        int newProposalId = testDB.savesDraftProposal("TestUser", title, descript, invitees, events, true, -1);
+        assertEquals(1, newProposalId);
+
+        // send the proposal
+        boolean status = testDB.sendProposal(1);
+        assertEquals(true, status);
+
+        // Remove event 1 from sent proposal
+        testDB.removeEventFromSentProposal(1, 1);
+
+        // Try to remove event 1 again from sent proposal
+        Mockito.when(request.getParameter("proposalId")).thenReturn("1");
+        Mockito.when(request.getParameter("eventId")).thenReturn("1");
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        Mockito.when(response.getWriter()).thenReturn(pw);
+        RemoveEventFromSentProposalServlet removeEventFromSentProposalServlet = new RemoveEventFromSentProposalServlet();
+        removeEventFromSentProposalServlet.init(config);
+        removeEventFromSentProposalServlet.doPost(request, response);
+        String result = sw.getBuffer().toString();
+        assertEquals("false", result);
+    }
+
+    @Test
+    public void testDoPostException() throws Exception {
+        HttpServletResponse failingResponse = Mockito.mock(HttpServletResponse.class);
+        HttpServletRequest failingRequest = Mockito.mock(HttpServletRequest.class);
+        RemoveEventFromSentProposalServlet removeEventFromSentProposalServlet = new RemoveEventFromSentProposalServlet();
+        removeEventFromSentProposalServlet.init(config);
+
+        Mockito.when(failingResponse.getWriter()).thenThrow(IOException.class);
+
+        try {
+            removeEventFromSentProposalServlet.doPost(failingRequest, failingResponse);
+            fail("Expected a Servlet Exception to be thrown");
+        } catch (ServletException servletException) {
+            assertEquals("Remove Event From Sent Proposal Servlet failed", servletException.getMessage());
+        }
     }
 }
