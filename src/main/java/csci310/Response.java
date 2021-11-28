010f2c097bf37fe1b50d847d48054c99d979abc9
Jiating Lu <jiatinglu99@gmail.com>
2021-11-27 23:57:55 -0800

    add Response class for data storage

---
+++ src/main/java/csci310/Response.java
@@ -0,0 +1,9 @@
+package csci310;
+
+public class Response {
+    public int userId;
+    public String userName;
+    public String availability;
+    public int excitement;
+    public Response(){};
+}
