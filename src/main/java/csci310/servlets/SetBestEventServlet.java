ffcfdaf2d414c401fd67d5f9f4ee46f7c692d362
Jiating Lu <jiatinglu99@gmail.com>
2021-11-28 19:55:18 -0800

    add skeleton and test code for all response-related servlets

---
+++ src/main/java/csci310/servlets/SetBestEventServlet.java
@@ -0,0 +1,18 @@
+package csci310.servlets;
+
+import csci310.Database;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class SetBestEventServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    }
+}
