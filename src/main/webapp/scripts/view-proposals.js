99781bbfe8d605927864ecc99e1506460dffc717
Elizabeth Gonzalez <gonz023@usc.edu>
2021-12-01 14:23:42 -0800

    update code for initial merge with develop

--- src/main/webapp/scripts/view-proposals.js
+++ src/main/webapp/scripts/view-proposals.js
@@ -16,6 +16,7 @@ let REC_PROPOSALS_DRAFT = []
 
 
 // Retrieve all proposals on window load
+/*
 window.onload = function() {
     $.ajax({
         method: "POST",
@@ -53,14 +54,14 @@ window.onload = function() {
       }
     })
 }
-
+*/
 // Handle button toggle on click to change button color and call filter/sort functions
 function toggle(b) {
     b.value = (b.value=="ON")?"OFF":"ON"
     if (b.value == "ON") {
         b.className = "on"
-        if (b.textContent == "finalized") displayResults(REC_PROPOSALS_FINAL)
-        else if (b.textContent == "unfinalized") displayResults(REC_PROPOSALS_DRAFT)
+        //if (b.textContent == "finalized") filterResults(final, REC_PROPOSALS_FINAL)
+        //else if (b.textContent == "unfinalized")
     }
     else {
         b.className = "off"
@@ -68,34 +69,50 @@ function toggle(b) {
 }
 
 function clearFilters() {
-    displayResults(REC_PROPOSALS_FINAL)
-    displayResults(REC_PROPOSALS_DRAFT)
+    //displayResults(REC_PROPOSALS_FINAL)
     document.querySelectorAll('.on').forEach((item) => {
            item.className = "off"
     })
 }
 
+function filterResults(type, proposalId) {
+    // Check column is_finalized and remove from list if is_finalized != input type
+}
+
 // Parse results into card display for HTML
 // Received proposals: Display title of proposal and
 function displayResults(filteredResults) {
-    for (i = 0; i < filteredResults.length; i++) {
+    filteredResults.forEach(result => {
         let resultsCard = `
-              <div class="border">
-              // TODO: Get Proposal Name from DB
-              //  <h4>${filteredResults[i].proposalId}</h4>
-                <h3>events</h3>
-                // TODO: Get list of events from db
-                  <ul><a href="#">Event 1</a></ul>
-                  <ul><a href="#">Event 2</a></ul>
-                <h3>date/time:</h3>
-                // TODO: Get time if finalized, or print 'not finalized'
-                <ul><p class="strong-result">not finalized</p></ul>
-              </div>
+            <div class="border">
+             // Get Proposal Name from DB
+               <h4>${result.proposalId.name}</h4>
+               <h3>events</h3>
+               // Get list of events from db
+                 <ul><a href="#">${result.proposalId.eventId}</a></ul>
+               <h3>date/time:</h3>
         `
-    }
+        if (${result.proposalId.is_finalized} == false) {
+            let resultsCard += `
+                    <ul><p class="strong-result" style="color:darkred;">not finalized</p></ul>
+                </div>
+            `
+        } else {
+            let resultsCard += `
+                      <ul><p class="strong-result" style="color:goldenrod;">${result.proposalId.best_event_id}</p></ul>
+                      <ul><p class="strong-result" style="color:goldenrod;">${result.proposalId.best_event_id.name.startDateTime.substring(0,10)}</p></ul>
+
+                </div>
+            `
+        }
+
+    })
 }
 
-// TODO: Sort events by start date
+function isFinalized(proposal) {
+
+}
+
 function sortResults() {
 }
 
